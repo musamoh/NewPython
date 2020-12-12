74fcd1f7fdfaf86c05da7372f09d11386530467e
musamoh <musamoh2007@gmail.com>
2020-12-12 15:39:38 +1100

    first commit

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+adj = input("Adjective ")
+verb = input("Verb ")
+famous = input("famous person ")
+
+
+
+
+sentense = f"i {adj} {verb} the guiter because i learnt it from {famous}".format(adj,verb,famous)
+#sentense = f"i {adj} {verb} the guiter because i learnt it from {famous}"
+
+print(sentense)
